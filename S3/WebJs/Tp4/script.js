51686cf673314bdfa54109b76453879eab1dbbe1
ClementZ <clement.zavodszki@gmail.com>
2021-10-05 21:29:08 +0200

    Toujours le tp4 de javascript

--- S3/WebJs/Tp4/script.js
+++ S3/WebJs/Tp4/script.js
@@ -1,20 +1,32 @@
+function createTable(json){
+    let tab=json['students']; //Recuperation du tableau students du fichier Json
+    let liste = tab.map(id=>{
+        let nom = id['firstname'];
+        let prenom =id['lastname'];
+        let avatar = "<td><img src='https://www.cril.univ-artois.fr/~lecoutre/animals/000.png'/></td>";
+        let res = ["<td>"+nom+"</td>", "<td>"+prenom+"</td>", avatar];
+        return res;
+    });
+    for (let i =0;i<liste.length;i++){
+        let tr = document.createElement("tr");
+        for (let j=0; j <liste[i].length;j++){
+            tr.innerHTML+=liste[i][j];
+            console.log(liste[i][j])
+        }
+        document.getElementById('student').appendChild(tr);
+    }
+}
 const req = new XMLHttpRequest();
 req.addEventListener("load", evt => {
     let data = JSON.parse(req.responseText);
-    console.log(data);
-    let tab = data['students'];
-    tab.map(id=>{
-        for (let i=0; i <id.length;i++){
-            tableEtu
-        }
-    })
-    console.log(tab);
+    createTable(data);
 });
 req.open("GET", "https://www.cril.univ-artois.fr/~lecoutre/students.json");
 req.send();
 
+
+
 let tableEtu = document.getElementById("student");
 let champ = document.querySelector("input");
 
-console.log(tableEtu, champ);
 tableEtu.innerHTML="<tr><th>First Name</th><th>Last Name</th><th>Avatar</th></tr>";
